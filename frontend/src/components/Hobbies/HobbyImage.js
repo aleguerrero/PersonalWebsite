0c23b3c762bb58ce7cb6e8aa070c78e4084241e3
Alejandro Guerrero A <a.guerrero.aguilar@accenture.com>
2023-07-18 19:18:12 -0600

    loading images in hobbies page

---
+++ frontend/src/components/Hobbies/HobbyImage.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function HobbyImage(props) {
+
+    const [currentImage, setCurrentImage] = useState();
+
+    import (`../Hobbies/images/${props.imagePath}`).then((image) =>
+        setCurrentImage(image.default)
+    );
+
+    return (
+        <div className='hobbyImage'>
+            <img src={currentImage} alt='test' />
+        </div>
+    );
+}
+
+export default HobbyImage;
