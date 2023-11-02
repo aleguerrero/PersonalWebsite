0ec3d34c42293f583cdcc36ad54caead772c789d
Alejandro Guerrero <alegue17@gmail.com>
2023-11-02 06:26:52 -0600

    working on about me page responsive

--- frontend/src/components/AboutMe/AboutMe.js
+++ frontend/src/components/AboutMe/AboutMe.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import "./AboutMe.css"
-import image from "./images/CL032.jpg" 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import image from "./images/CL032.jpg"
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 function AboutMe() {
 
@@ -15,22 +13,19 @@ function AboutMe() {
 
     return (
         <Container className='AboutMeBlock'>
-            <div className='myImage'>
-                <img src={image} alt="Looking good" />
-            </div>
-            <div className='aboutMeText'>
-                <Row>
-                    <Col>
-                        <h5>
-                            Hello! Welcome to my website!
-                        </h5>
-                    </Col>
-                </Row>
-                <p>
-                    I'm Alejandro Guerrero Aguilar, I'm a {getDiffYears(new Date("05/17/1998"))} years old Software Engineer from San José, Costa Rica, with {getDiffYears(new Date("01/01/2016"))} years of experience.
-                    I am deeply appasionate for computers and how do they work to run stuff, so that is why I took myself to this career, challenging myself everyday to learn something new
-                    professionally and personally.<br/> <br/>
-                    I'm also a big fan of</p>
+            <Row>
+                <Col xxl={4} className='myImage'>
+                    <Image src={image} alt="Looking good" fluid/>
+                </Col>
+                <Col xxl={8} className='aboutMeText'>
+                    <h5>
+                        Hello! Welcome to my website!
+                    </h5>
+                    <p>
+                        I'm Alejandro Guerrero Aguilar, I'm a {getDiffYears(new Date("05/17/1998"))} years old Software Engineer from San José, Costa Rica, with {getDiffYears(new Date("01/01/2016"))} years of experience.
+                        I am deeply appasionate for computers and how do they work to run stuff, so that is why I took myself to this career, challenging myself everyday to learn something new
+                        professionally and personally.<br /> <br />
+                        I'm also a big fan of</p>
                     <ul className='hobbiesList'>
                         <li>
                             Cars
@@ -43,16 +38,17 @@ function AboutMe() {
                         </li>
                     </ul>
                     <p>
-                        My hobbies are big part of what I like to do and what I am and what keeps me 
-                    pushing everyday to learn more, explore new experiences, meet new people, and develop myself personally.
+                        My hobbies are big part of what I like to do and what I am and what keeps me
+                        pushing everyday to learn more, explore new experiences, meet new people, and develop myself personally.
                     </p>
                     <p>
                         I made this website because I wanted to challange myself with JavaScript and it's frameworks, and CSS, since I haven't used before JavaScript and CSS.
-                        Also I like having a website where I can promote myself and my work. 
-                        <br/><br/>
+                        Also I like having a website where I can promote myself and my work.
+                        <br /><br />
                         For this website, I am using ReactJS in the front-end, Express for the back-end and MongoDB for the database.
                     </p>
-            </div>
+                </Col>
+            </Row>
         </Container>
     );
 }
